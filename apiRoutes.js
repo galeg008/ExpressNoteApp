c39a4c06fe7f86548547ca0f78d4adcf3b12c598
giovanni <giovanni_gal08@yahoo.com>
2023-01-31 22:25:53 -0800

    first commit

---
+++ apiRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+router.get('/', (req,res)=> {
+    //get all the notes from DB
+})
+
+router.post('/', (req,res)=> {
+    //add a note to the DB
+});
+
+
+
+//bonus is to be able to delete
+module.exports = router;
